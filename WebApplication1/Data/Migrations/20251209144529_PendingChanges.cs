abc3eaa059df55ce25ce7434a650902a15de7725
ShigureJishin <chungten2000@gmail.com>
2025-12-10 00:53:50 +0800

    新增: 實作 Web 匯入頁面，支援單筆資料及檔案上傳寫入資料庫

---
+++ WebApplication1/Data/Migrations/20251209144529_PendingChanges.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
